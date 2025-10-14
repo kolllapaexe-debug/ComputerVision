adac88f36a8e4818f550f1178f99c768d733da6e
Баранов Єгор <kolllapa.exe@gmail.com>
2025-10-14 12:03:07 +0300

    Add files via upload

---
+++ CW_lesson_6.py
@@ -0,0 +1,25 @@
+import cv2
+cap = cv2.VideoCapture(0)
+ret, frame1 = cap.read()
+gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+gray1 = cv2.convertScaleAbs(gray1, alpha=2, beta=5)
+while True:
+    ret, frame2 = cap.read()
+    if not ret:
+        print("video capture failed")
+        break
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.convertScaleAbs(gray2, alpha=2, beta=5)
+    diff = cv2.absdiff(gray1, gray2)
+    _, thresh = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if cv2.contourArea(cnt)>1300:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(frame2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame2)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
