482ad54cb9247bdde3fa39bc9da3c1d5b6acc1b5
Баранов Єгор <kolllapa.exe@gmail.com>
2025-10-16 10:14:23 +0300

    Add files via upload

---
+++ CW_lesson_7.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture(0)
+lower_red1 = np.array([0, 100, 100])
+upper_red1 = np.array([30, 255, 255])
+
+lower_red2 = np.array([160, 100, 100])
+upper_red2 = np.array([180, 255, 255])
+
+points = []
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("video capture failed")
+        break
+    frame = cv2.flip(frame,1)
+    HSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    mask1 = cv2.inRange(HSV, lower_red1, upper_red1)
+    mask2 = cv2.inRange(HSV, lower_red2, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            cv2.rectangle(frame, (0, 0), (680, 480), (0, 255, 0), 2)
+            cv2.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+            cv2.putText(frame, f'Object appear', (13,13), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
+        else:
+            cv2.rectangle(frame, (0, 0), (680, 480), (0, 0, 255), 2)
+            cv2.putText(frame, f'No object found', (13, 13), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
+            # M = cv2.moments(cnt)
+    #         if M["m00"] != 0:
+    #             cx = int(M["m10"] / M["m00"])
+    #             cy = int(M["m01"] / M["m00"])
+    #             cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
+    #             points.append((cx, cy))
+    # for i in range(1, len(points)):
+    #     if points[i-1][0] is None or points[i] is None:
+    #         continue
+    #     cv2.line(frame, points[i-1], points[i], (0, 0, 255), 2)
+    cv2.imshow('frame', mask)
+    cv2.imshow('original', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
